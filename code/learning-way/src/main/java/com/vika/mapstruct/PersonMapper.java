d6026c987e19f668b4446779d576431d9c38a3a5
tangjiawei <332452274@qq.com>
2020-08-27 11:18:42 +0800

    mapstruct

--- code/learning-way/src/main/java/com/vika/mapstruct/PersonMapper.java
+++ code/learning-way/src/main/java/com/vika/mapstruct/PersonMapper.java
@@ -1,8 +1,23 @@
 package com.vika.mapstruct;
 
+import com.vika.reflection.User;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
 /**
  * @Author tangjiawei
  * @Date 2020/8/27
  */
+@Mapper
 public interface PersonMapper {
+
+    PersonMapper INSTANCE = Mappers.getMapper(PersonMapper.class);
+
+    /**
+     * 转换
+     *
+     * @param person
+     * @return
+     */
+    User toUser(Person person);
 }
