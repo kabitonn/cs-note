4e259499d4b85aac9f00709cd9698fbbed62bb3c
tangjiawei <332452274@qq.com>
2020-09-01 23:01:04 +0800

    tencent interview

--- code/learning-way/src/main/java/com/vika/algorithm/leetcode/common/TreeNode.java
+++ code/learning-way/src/main/java/com/vika/algorithm/leetcode/common/TreeNode.java
@@ -1,8 +1,16 @@
 package com.vika.algorithm.leetcode.common;
 
 public class TreeNode {
-	public int val;
-	public TreeNode left;
-	public TreeNode right;
-	public TreeNode(int x) { val = x; }
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+
+    public TreeNode(int x) {
+        val = x;
+    }
+
+    @Override
+    public String toString() {
+        return val + "left:" + left + "right" + right;
+    }
 }
---
+++ code/learning-way/src/main/java/com/vika/autumn/exam/tencent/BinarySearch.java
@@ -0,0 +1,33 @@
+package com.vika.autumn.exam.tencent;
+
+import org.junit.Test;
+
+/**
+ * @Author tangjiawei
+ * @Date 2020/9/1
+ */
+public class BinarySearch {
+
+    public int binarySearch(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length;
+        while (left <= right) {
+            int mid = (left + right) >>> 1;
+            if (mid > target) {
+                right = mid - 1;
+            } else if (mid < target) {
+                left = mid + 1;
+            } else {
+                return mid;
+            }
+        }
+        return -1;
+    }
+
+    @Test
+    public void test() {
+        int[] nums = {1, 2, 3, 4, 5, 6};
+        int index = binarySearch(nums, 3);
+        System.out.println(index);
+    }
+}
